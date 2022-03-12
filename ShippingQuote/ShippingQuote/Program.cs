4860c7d29c47b035f9f6ba767abacd4f6d2d1d31
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-12 12:28:42 -0700

    Created simple program to determine shipping cost of package

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:"); // Prompts user to enter package weight
+            int weight = Convert.ToInt32(Console.ReadLine()); // Converts input to an Int and stores it as weight
+            if (weight > 50) // This block of code will be run if weight is over 50
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0); // Closes console to prevent program from continuing
+            }
+
+            Console.WriteLine("Please enter the package width:"); // Prompts user to enter package width
+            int width = Convert.ToInt32(Console.ReadLine()); // Converts input to an Int and stores it as width
+            if (width > 50) // This block of code will be run if width is over 50
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0); // Closes console to prevent program from continuing
+            }
+
+            Console.WriteLine("Please enter the package height:"); // Prompts user to enter package height
+            int height = Convert.ToInt32(Console.ReadLine()); // Converts input to an Int and stores it as hieght
+            if (height > 50) // This block of code will be run if height is over 50
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0); // Closes console to prevent program from continuing
+            }
+
+            Console.WriteLine("Please enter the package length:"); // Prompts user to enter package length
+            int length = Convert.ToInt32(Console.ReadLine()); // Converts input to an Int and stores it as length
+            if (length > 50) // This block of code will be run if length is over 50
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0); // Closes console to prevent program from continuing
+            }
+
+            int quote = ((height * width * length) * weight) / 100; // Function that aqcuires the shipping estimate and stores it as quote
+            Console.WriteLine("Your estimated total for shipping this package is: " + quote.ToString("$#,0.00")); // Prints shipping estimate with a specific format. quote is converted to String here for proper display
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+        }
+    }
+}
