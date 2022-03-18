8ba805c042ab0750c6f5654e8ae06e515e7adb10
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-18 14:47:46 -0600

    Added dividebyzero exception and changed double to decimal type

--- StringAndIntegers/StringAndIntegers/Program.cs
+++ StringAndIntegers/StringAndIntegers/Program.cs
@@ -10,32 +10,36 @@ namespace StringAndIntegers
     {
         static void Main(string[] args)
         {
-            List<double> intList = new List<double>();
-            intList.Add(2.0);
-            intList.Add(5.0);
-            intList.Add(7.0);
-            intList.Add(10.0);
-            intList.Add(11.0);
-            intList.Add(14.0);
-            intList.Add(22.0);
-            intList.Add(24.0);
-            intList.Add(1005.0);
-            intList.Add(5000.0);
+            List<decimal> intList = new List<decimal>();
+            intList.Add(2);
+            intList.Add(5);
+            intList.Add(7);
+            intList.Add(10);
+            intList.Add(11);
+            intList.Add(14);
+            intList.Add(22);
+            intList.Add(24);
+            intList.Add(1005);
+            intList.Add(5000);
 
             
             try
             {
                 Console.WriteLine("Please provide a number to divide each number in the list by:");
-                double input = Convert.ToDouble(Console.ReadLine()); // Converts user input into Double and stores it as input
+                decimal input = Convert.ToDecimal(Console.ReadLine()); // Converts user input into Decimal and stores it as input
                 for (int i = 0; i < intList.Count; i++)
                 {
                     var result = intList[i] / input; // Function that divides user input by each item in the list
                     Console.WriteLine(intList[i] + " divided by " + input + " equals " + result);
                 }
             }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please do not divide by zero. " + ex);
+            }
             catch (FormatException ex) // Raises exception if user types a string
             {
-                Console.WriteLine("Please type a number" + ex);
+                Console.WriteLine("Please type a number " + ex);
             }
             finally // This block of code is executed at the end of the loop regardless of outcome
             {
