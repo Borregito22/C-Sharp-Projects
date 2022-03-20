bab69bfa57c2953b9cd09fe6daa5d1471dccc515
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-20 13:08:28 -0600

    Created program that uses three methods with the same name

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Math
+    {
+        public static int SimpleFunction(int num1) // Public method named SimpleFunction with an int parameter
+        {
+            int result; // Creates variable with int data type
+            result = num1 + 15; // Function that uses parameter and stores value under result
+            return result;
+        }
+
+        public static int SimpleFunction(decimal num1) // Parameter is decimal datatype
+        {
+            int result;
+            result = Convert.ToInt32(num1) * 3; // Converts parameter to int data type to return result as int
+            return result;
+        }
+
+        public static int SimpleFunction(string num1) // Parameter is string data type
+        {
+            int result;
+            result = Convert.ToInt32(num1) / 4; // Converts parameter into int data type to be able to do math to it
+            return result;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 35; // Creates int variable
+            Console.WriteLine(input + " plus fifteen equals " + Math.SimpleFunction(input)); // Pulls method SimpleFunction from class Math and uses input as parameter
+            Console.ReadLine();
+
+            decimal input2 = 40.2m; // Creates decimal variable
+            Console.WriteLine(input2 + " times three equals " + Math.SimpleFunction(input2)); // Pulls method SimpleFunction from class Math and uses input2 as parameter
+            Console.ReadLine();
+
+            string input3 = "50"; // Creates string variable
+            Console.WriteLine(input3 + " divided by four equals " + Math.SimpleFunction(input3)); // Pulls method SimpleFunction from class Math and uses input3 as parameter
+            Console.ReadLine();
+        }
+    }
+}
