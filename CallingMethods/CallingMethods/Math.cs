0dea521702aa61b04fb3cbd7a4341cc509271ee9
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-19 18:24:08 -0600

    Created program that calls methods from a different class

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Math
+    {
+        static public int DivideTwelve(int num1) // Function named DivideTwelve with int data type, num1 is the parameter and is int data type
+        {
+            int result; // Creates integer variable called result
+            result = num1 / 12; // Divides parameter by 12 and stores it as result
+            return result; // Reterns result 
+        }
+
+        static public int TimesTwentyTwo(int num1)
+        {
+            int result;
+            result = num1 * 22;
+            return result;
+        }
+
+        static public int TimesFourtyFive(int num1)
+        {
+            int result;
+            result = num1 * 45;
+            return result;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would do you want to do math operations on?");
+            int input = Convert.ToInt32(Console.ReadLine()); // Converts user input into Int and stores it as input
+
+            Console.WriteLine(input + " divided by twelve equals " + Math.DivideTwelve(input)); // Calls method DivideTwelve from class Math and uses input as perameter
+            Console.WriteLine(input + " multiplied by fourty five equals " + Math.TimesFourtyFive(input)); // Calls method TimesFourtyFive from class Math and uses input as perameter
+            Console.WriteLine(input + " multiplied by twenty two equals " + Math.TimesTwentyTwo(input)); // Calls method TimesTwentyTwo from class Math and uses input as perameter
+            Console.ReadLine();
+        }
+    }
+}
