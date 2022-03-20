a6fc6e9f22beb395f1a2e907cd7c767254c6fc0b
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-20 17:28:57 -0600

    Created program that has an optional parameter

---
+++ Method/Method/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Math
+    {
+        public int OptionalParameterFunction(int num1, int num2 = 7) // Creates two parameters with int data types. num2 has a default value
+        {
+            int result;
+            result = num1 * num2;
+            return result;
+        }
+    }
+}
---
+++ Method/Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); // Instantiates the class
+
+            int input, input2, result; // Creates 3 variables with integer data types
+
+            Console.WriteLine("Please pick a number:");
+            input = Convert.ToInt32(Console.ReadLine()); // Converts user input into an integer and stores it as input
+
+            Console.WriteLine("Please pick another number. This is optional:");
+            if (int.TryParse(Console.ReadLine(), out input2)) // Checks to see if user entered anything
+            {
+                result = math.OptionalParameterFunction(num1: input, num2: input2); // Passes both input and input2 as parameters
+            }
+            else
+            {
+                result = math.OptionalParameterFunction(input); // Only uses input as parameter and uses default value for input2
+            }
+
+            Console.WriteLine(input + " multiplied by " + input2 + " equals " + result);
+            Console.ReadLine();
+        }
+    }
+}
