5fc14fd1c59222ac1adbc054444dc417a29e9f6b
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-21 14:08:14 -0600

    Created program that inherits another class

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+        public string FirstName; // Creates a property called FirstName with string data type
+        public string LastName;
+
+        public void SayName() // Void method to write the person's full name
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
