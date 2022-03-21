1d99af4404e1944b4283009b65cd3d5dc17a55e1
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-21 13:40:07 -0600

    Program that has a void method

---
+++ Class/Class/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Math
+    {
+        public void Divide(int num1)
+        {
+            int result;
+            result = num1 / 2;
+            Console.WriteLine(result);
+            return;
+        }
+
+        public static void OutputMethod(out int num1, out int num2) // Method with output parameters, Class is declared static
+        {
+            num1 = 7;
+            num2 = 643;
+        }
+
+        public static void OutputMethod(out int num1, out int num2, out int num3) // Overloads OutputMethod
+        {
+            num1 = 7;
+            num2 = 643;
+            num3 = 3450;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); // Instantiates the class
+
+            Console.WriteLine("Please enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine()); // Stores user input as input as an integer
+
+            math.Divide(input); // Passes input as parameter to class method
+            Console.ReadLine();
+
+
+        }
+    }
+}
