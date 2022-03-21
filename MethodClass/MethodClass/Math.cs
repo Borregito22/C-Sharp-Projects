8747711f7542ec5dd47f8ac00597f376c894eb1d
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-21 13:14:01 -0600

    Created program that takes two parameters

---
+++ MethodClass/MethodClass/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Math
+    {
+        public void Display(int num1, int num2) // Void method with 2 parameters
+        {
+            int result;
+            result = num1 * 17; // Function that stores value as result
+            Console.WriteLine(num2); // Prints value of num2 to console
+            return;
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); // Instantiates the class
+            int input = 7; // Creates integer variable
+            int input2 = 15;
+
+            math.Display(num1: input, num2: input2); // Passes input and input2 as parameters for the class method
+            Console.ReadLine();
+        }
+    }
+}
