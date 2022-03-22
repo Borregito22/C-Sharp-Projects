c975b8edc3dd0d1ac959c5dacabbeda8960b44a3
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-22 13:05:00 -0600

    Created program that overloads == operator

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee
+    {
+        public int Id; // Creates integer variable called Id
+        
+        public Employee(int x) // Method that allows instantiation of an object with a property
+        {
+            Id = x;
+        }
+
+        public static bool operator == (Employee employee1, Employee employee2) // Overloaded == operator to compare employee1 to employee2
+        {
+            if (employee1.Id == employee2.Id) // Compares Id's of employee1 and employee2
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2) // Comparison operators must be overloaded in pairs
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(1); // Instantiates Employee class with new object and passes a parameter
+            Employee employee2 = new Employee(2);
+            
+            if (employee1 == employee2) // If loop that compares Id's between employee1 and employee2
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
