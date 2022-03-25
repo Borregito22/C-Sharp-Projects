8be4f922dba20e471e0679a22ca1148a8e9fc336
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-25 11:36:22 -0600

    Created program with a struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number(); // Instantiates Number class with a new object
+            number.Amount = 7; // Sets value to Amount property in Number.number
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
