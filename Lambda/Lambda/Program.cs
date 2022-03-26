09b9389d89102642ca7dd81c70b33af09743dd26
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2022-03-25 18:50:28 -0600

    Updated code

--- Lambda/Lambda/Program.cs
+++ Lambda/Lambda/Program.cs
@@ -11,29 +11,47 @@ namespace Lambda
         static void Main(string[] args)
         {
             List<Employee> employees = new List<Employee>();
-            employees.Add(new Tuple<string, string, int>("Joe", "Smith", 1));
-            employees.Add(new Tuple<string, string, int>("John", "Johnson", 2));
-            employees.Add(new Tuple<string, string, int>("Bill", "Brown", 3));
-            employees.Add(new Tuple<string, string, int>("Tom", "Williams", 4));
-            employees.Add(new Tuple<string, string, int>("Jane", "Jones", 5));
-            employees.Add(new Tuple<string, string, int>("Joe", "Garcia", 6));
-            employees.Add(new Tuple<string, string, int>("Mary", "Miller", 7));
-            employees.Add(new Tuple<string, string, int>("Sarah", "Davis", 8));
-            employees.Add(new Tuple<string, string, int>("Lisa", "Martinez", 9));
-            employees.Add(new Tuple<string, string, int>("Sandra", "Anderson", 10));
+
+            new Employee() { Id = 1, firstName = "Joe", lastName = "Schmo" };
+            new Employee() { Id = 2, firstName = "Andrew", lastName = "Schmo" };
+            new Employee() { Id = 8, firstName = "Joe", lastName = "Schmo" };
+
+            //employees.Add(new Tuple<string, string, int>("Joe", "Smith", 1));
+            //employees.Add(new Tuple<string, string, int>("John", "Johnson", 2));
+            //employees.Add(new Tuple<string, string, int>("Bill", "Brown", 3));
+            //employees.Add(new Tuple<string, string, int>("Tom", "Williams", 4));
+            //employees.Add(new Tuple<string, string, int>("Jane", "Jones", 5));
+            //employees.Add(new Tuple<string, string, int>("Joe", "Garcia", 6));
+            //employees.Add(new Tuple<string, string, int>("Mary", "Miller", 7));
+            //employees.Add(new Tuple<string, string, int>("Sarah", "Davis", 8));
+            //employees.Add(new Tuple<string, string, int>("Lisa", "Martinez", 9));
+            //employees.Add(new Tuple<string, string, int>("Sandra", "Anderson", 10));
 
             List<Employee> Joe = new List<Employee>();
+
             foreach (var employee in employees)
             {
-                if (employee.Item1 == "Joe")
+                if (employee.firstName == "Joe")
                 {
-                    Console.WriteLine("{0} {1} {2}", employee.Item1, employee.Item2, employee.Item3);
+                    Joe.Add(employee);
+                    Console.WriteLine("{0} {1} {2}", employee.firstName, employee.lastName, employee.Id);
+                    Console.ReadLine();
                 }
             }
 
-            employees.ForEach(x => x.Item1 == "Joe").ToList();
-            
+            List<Employee> Joe2 = employees.Where(x => x.firstName == "Joe").ToList();
+            Joe2.ForEach(x => Console.WriteLine(x.firstName));
+
+            List<Employee> Joe3 = employees.Where(x => x.Id > 5).ToList();
+            Joe3.ForEach(x => Console.WriteLine(x.firstName));
+
             Console.ReadLine();
+        }
+        public class Employee
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int Id { get; set; }
         }
     }
 }
